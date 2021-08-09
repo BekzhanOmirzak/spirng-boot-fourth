36a7ac559b1d469e8d056421712b60b8aed592d3
Bekzhan <bekjan.omirzak98@gmail.com>
2021-08-09 20:45:48 +0600

    Fifth message

--- src/main/java/springbootthird/third/controller/MainController.java
+++ src/main/java/springbootthird/third/controller/MainController.java
@@ -4,20 +4,24 @@ package springbootthird.third.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springbootthird.third.models.User;
 import springbootthird.third.repository.UserRepo;
 
 @RestController
 public class MainController {
 
+
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping()
-    public Iterable<User> getUserList() {
-        return userRepo.findAll();
+    @GetMapping("/users")
+    public String getUserString() {
+        return "users";
     }
 
+    @GetMapping("/posts")
+    public String getPosts() {
+        return "These are the posts provided by me";
+    }
 
 
 
